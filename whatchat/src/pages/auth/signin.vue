50b214db4a63dce7a4fb6fbca083dcff5637cbaf
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2019-08-13 15:51:18 +0300

    Created sign in action

--- whatchat/src/pages/auth/signin.vue
+++ whatchat/src/pages/auth/signin.vue
@@ -1,35 +1,42 @@
 <template>
-  <f7-page name="signin">
+<f7-page name="signin">
     <f7-navbar title="Sign In" back-link="Back"></f7-navbar>
 
     <f7-list no-hairlines-md>
-      <f7-list-input label="E-mail" :value="email"  @input="email=$event.target.value" type="email" placeholder="Your e-mail" clear-button>
-      </f7-list-input>
+        <f7-list-input label="E-mail" :value="email" @input="email=$event.target.value" type="email" placeholder="Your e-mail" clear-button>
+        </f7-list-input>
 
-      <f7-list-input label="Password" :value="password"  @input="password=$event.target.value" type="password" placeholder="Your password" clear-button>
-      </f7-list-input>
+        <f7-list-input label="Password" :value="password" @input="password=$event.target.value" type="password" placeholder="Your password" clear-button>
+        </f7-list-input>
     </f7-list>
-    
+
     <f7-block>
-      <f7-button outline>Sign In </f7-button>
-      <br>
-      <div style="text-align:center">
-        <f7-link>Resend Confirmation Email</f7-link><br>
-        <f7-link href="/signup/">Don't have an account? Sign Up</f7-link><br>
-        <f7-link>Forgot Password</f7-link><br>
-      </div> 
+        <f7-button outline @click="signIn">Sign In </f7-button>
+        <br>
+        <div style="text-align:center">
+            <f7-link>Resend Confirmation Email</f7-link><br>
+            <f7-link href="/signup/">Don't have an account? Sign Up</f7-link><br>
+            <f7-link>Forgot Password</f7-link><br>
+        </div>
     </f7-block>
-  </f7-page>
+</f7-page>
 </template>
 
 <script>
 export default {
-  data(){
-    return {
-      email:null,
-      password:null
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
+    methods: {
+        signIn() {
+            var payload = {}
+            payload.email = this.email
+            payload.password = this.password
+            this.$store.dispatch('signIn', payload)
+        }
     }
-
-  }
 }
 </script>
--- whatchat/src/pages/store/AuthModule.js
+++ whatchat/src/pages/store/AuthModule.js
@@ -2,27 +2,50 @@ import firebase from 'firebase';
 
 const AuthModule = {
     state: {
+        signed_in: false,
         signed_up: false
     },
     getters: {
+        signed_in: state => state.signed_in,
         signed_up: state => state.signed_up
     },
     mutations: {
+        setSignedIn(state, payload) {
+            state.signed_in = payload
+        },
         setSignedUp(state, payload) {
             state.signed_up = payload
         }
     },
     actions: {
+        signIn({ commit }, payload) {
+            firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+                .then(user => {
+                    //logined in
+                    commit('setSignedIn', true)
+                })
+                .catch(function (error) {
+                    // Handle Errors here.
+                    commit('setAlertMessage', error)
+                    // ...
+                });
+        },
         signUp({ commit, dispatch }, payload) {
-            firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(payload.email, payload.password)
                 .then(data => {
-                    firebase.database().ref('users').child(data.user.uid).set({
-                        uid: data.user.uid,
-                        name: payload.name,
-                        email: payload.email,
-                        emailverified: false,
-                        photo_url: payload.photoURL
-                    });
+                    firebase
+                        .database()
+                        .ref('users')
+                        .child(data.user.uid)
+                        .set({
+                            uid: data.user.uid,
+                            name: payload.name,
+                            email: payload.email,
+                            emailverified: false,
+                            photo_url: payload.photoURL
+                        });
                     let newUser = data.user;
                     newUser.updateProfile({
                         displayName: payload.name,
