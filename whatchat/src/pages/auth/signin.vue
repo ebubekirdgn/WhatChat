73d8b375489c5dc1c81b380dd42588fdddfe713b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2019-08-14 00:43:25 +0300

    Created count down on sending  verification email

--- whatchat/src/pages/auth/signin.vue
+++ whatchat/src/pages/auth/signin.vue
@@ -1,6 +1,6 @@
 <template>
 <f7-page name="signin">
-    <f7-navbar title="Sign In" back-link="Back"></f7-navbar>
+    <f7-navbar title="Sign In"></f7-navbar>
 
     <f7-list no-hairlines-md>
         <f7-list-input label="E-mail" :value="email" @input="email=$event.target.value" type="email" placeholder="Your e-mail" clear-button>
@@ -14,23 +14,50 @@
         <f7-button outline @click="signIn">Sign In </f7-button>
         <br>
         <div style="text-align:center">
-            <f7-link>Resend Confirmation Email</f7-link><br>
+            <f7-link @click="resendEmail">Resend Confirmation Email <span v-if="time_left > 0"></span> </f7-link><br>
             <f7-link href="/signup/">Don't have an account? Sign Up</f7-link><br>
-            <f7-link>Forgot Password</f7-link><br>
+            <f7-link>Forgot Password</f7-link>
+            <br>
+            {{time_left}}
         </div>
     </f7-block>
 </f7-page>
 </template>
 
 <script>
+import {
+    setInterval,
+    clearInterval
+} from 'timers';
 export default {
     data() {
         return {
             email: null,
-            password: null
+            password: null,
+            time_left: -1
         }
     },
     methods: {
+        resendEmail() {
+            const self = this
+
+            if (self.time_left <= 0) {
+                console.log('trigger resend')
+                self.$store.dispatch('sendVerification')
+                self.countDown()
+            }
+        },
+        countDown() {
+            const self = this
+            self.time_left = 20
+            var timer = setInterval(function () {
+                self.time_left -= 1
+                console.log('time_left', self.time_left)
+                if (self.time_left <= 0) {
+                    clearInterval(timer)
+                }
+            }, 1000)
+        },
         signIn() {
             var payload = {}
             payload.email = this.email
