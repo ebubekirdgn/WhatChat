bed663c84fe36b7e1fd16c7520729b6393ca4763
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2019-09-24 23:28:32 +0300

    Beautify the requests
    Beautify the requests

--- whatchat/src/pages/chat/requests.vue
+++ whatchat/src/pages/chat/requests.vue
@@ -1,13 +1,31 @@
 <template>
 <f7-page name="about">
     <f7-navbar title="Requests" back-link="Back"></f7-navbar>
-    <f7-block-title>About My App</f7-block-title>
-  <pre>{{friend_requests}}</pre>
+    <f7-block-title>Friend Requests</f7-block-title>
+
+    <f7-list media-list>
+        <f7-list-item swipeout v-for="(request, index) in friend_requests" :key="index" :title="request.name">
+            <img class="small-avatar" :src="request.photo_url" slot="media">
+            <f7-swipeout-actions right>
+                <f7-swipeout-button color="green" @click=" confirm(request)">Add</f7-swipeout-button>
+                <f7-swipeout-button color="red" @click=" remove(request)">Remove</f7-swipeout-button>
+            </f7-swipeout-actions>
+        </f7-list-item>
+    </f7-list>
+
 </f7-page>
 </template>
 
 <script>
 export default {
+    methods:{
+        confirm(request){
+
+        },
+        remove(request){
+
+        }
+    },
     computed: {
         friend_requests() {
             return this.$store.getters.friend_requests
@@ -19,3 +37,12 @@ export default {
     }
 }
 </script>>
+<style scoped> 
+.small-avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    object-fit: cover;
+    object-position: center;
+}
+</style>
