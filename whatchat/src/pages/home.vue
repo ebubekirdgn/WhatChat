46d602f7a6efb4ee0b3540f319317fcce4f69987
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2019-09-25 18:36:27 +0300

    Beautify the home page
    Beautify the home page

--- whatchat/src/pages/home.vue
+++ whatchat/src/pages/home.vue
@@ -18,49 +18,11 @@
         </f7-nav-right>
         <f7-nav-title-large sliding>WhatChat </f7-nav-title-large>
     </f7-navbar>
+    <f7-list media-list>
+        <f7-list-item v-for="(frd, index) in friends" :key="index" :title="frd.name">
+            <img class="small-avatar" :src="frd.photo_url" slot="media">
 
-    <!-- Page content-->
-    <pre>{{friends}}</pre>
-    <f7-block strong>
-        <p>This is an example of tabs-layout application. The main point of such tabbed layout is that each tab contains independent view with its own routing and navigation.</p>
-
-        <p>Each tab/view may have different layout, different navbar type (dynamic, fixed or static) or without navbar like this tab.</p>
-    </f7-block>
-
-    <f7-block-title>Navigation</f7-block-title>
-    <f7-list>
-        <f7-list-item link="/about/" title="About"></f7-list-item>
-        <f7-list-item link="/form/" title="Form"></f7-list-item>
-    </f7-list>
-
-    <f7-block-title>Modals</f7-block-title>
-    <f7-block strong>
-        <f7-row>
-            <f7-col width="50">
-                <f7-button fill raised popup-open="#my-popup">Popup</f7-button>
-            </f7-col>
-            <f7-col width="50">
-                <f7-button fill raised login-screen-open="#my-login-screen">Login Screen</f7-button>
-            </f7-col>
-        </f7-row>
-    </f7-block>
-
-    <f7-block-title>Panels</f7-block-title>
-    <f7-block strong>
-        <f7-row>
-            <f7-col width="50">
-                <f7-button fill raised panel-open="left">Left Panel</f7-button>
-            </f7-col>
-            <f7-col width="50">
-                <f7-button fill raised panel-open="right">Right Panel</f7-button>
-            </f7-col>
-        </f7-row>
-    </f7-block>
-
-    <f7-list>
-        <f7-list-item title="Dynamic (Component) Route" link="/dynamic-route/blog/45/post/125/?foo=bar#about"></f7-list-item>
-        <f7-list-item title="Default Route (404)" link="/load-something-that-doesnt-exist/"></f7-list-item>
-        <f7-list-item title="Request Data & Load" link="/request-and-load/user/123456/"></f7-list-item>
+        </f7-list-item>
     </f7-list>
 
 </f7-page>
@@ -82,3 +44,13 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.small-avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    object-fit: cover;
+    object-position: center;
+}
+</style>
